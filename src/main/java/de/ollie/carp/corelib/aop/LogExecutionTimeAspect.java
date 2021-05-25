61bb977d6d433bc8fd3ea24caa7ba458129fe294
ollie <lieshoff@gmx.de>
2021-05-25 11:06:15 +0200

    Intermediate check in.

--- src/main/java/de/ollie/carp/corelib/aop/LogExecutionTimeAspect.java
+++ src/main/java/de/ollie/carp/corelib/aop/LogExecutionTimeAspect.java
@@ -1,13 +1,20 @@
 package de.ollie.carp.corelib.aop;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.context.annotation.Configuration;
 
 @Aspect
 @Configuration
 public class LogExecutionTimeAspect {
+
+	@Before(value = "execution(* de.ollie.aspects.*.*(..))")
+	public void before(JoinPoint joinPoint) {
+		System.out.println("done something before excuting: " + joinPoint.getSignature());
+	}
 
 	@Around("@annotation(de.ollie.carp.corelib.aop.annotations.LogExecutionTime)")
 	public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
